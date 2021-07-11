af485b27ce1f3a1c5fcf7831a7813f8fc419a862
Mathiew Abbas <mathiew95@gmail.com>
2021-07-11 11:35:13 +0300

    Added intial windows support

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_screenshot/flutter_screenshot_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterScreenshotPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterScreenshotPlugin"));
+}
